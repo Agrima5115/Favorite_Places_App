f38230bd6aa5c1b8d2d1482990d681813d22890f
Agrima5115 <agrimarajput095@gmail.com>
2024-09-20 15:31:52 +0530

    added navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
-import 'package:favorite_places/screens/places.dart';
-import 'package:favorite_places/widgets/places_list.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:favorite_places/screens/places.dart';
 
 final colorScheme = ColorScheme.fromSeed(
   brightness: Brightness.dark,
@@ -28,7 +27,9 @@ final theme = ThemeData().copyWith(
 
 void main() {
   runApp(
-      const MyApp(),
+    const ProviderScope(
+      child: MyApp(),
+    ),
   );
 }
 
